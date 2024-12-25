ad027604fd99842b5f39e5db995837d0a9b561fe
Stanley Kiura <stanleykiura@Stanleys-MacBook-Air.local>
2024-12-26 01:52:35 +0300

    test2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Sir! This is a test from Matheka!"
+    return "Hello, Github! Please work!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
